228dbf0e25efd923a509de3c69a7655acb335b56
BenjaminFern <66424102+BenjaminFern@users.noreply.github.com>
2022-06-21 11:18:34 -0600

    Created weather api practice activity

---
+++ weather-api/js/weatherapi.js
@@ -0,0 +1,34 @@
+// Api Key
+
+// 7294e7be11cf3a02c424a31759e82f74
+// 64.835365, -147.776749
+// http://api.openweathermap.org/data/2.5/forecast?lat=64.835365&lon=-147.776749&appid=7294e7be11cf3a02c424a31759e82f74
+
+// select HTML elements to edit
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+// const url = "http://api.openweathermap.org/data/2.5/forecast?lat=64.835365&lon=-147.776749&appid=7294e7be11cf3a02c424a31759e82f74"
+const url = "http://api.openweathermap.org/data/2.5/forecast?q=Fairbanks&units=imperial&appid=7294e7be11cf3a02c424a31759e82f74"
+
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+
+        currentTemp.innerHTML = `<strong>${data["list"][0]["main"]["temp"].toFixed(0)}</strong>`;
+
+        const iconsrc = `https://openweathermap.org/img/w/${data["list"][0]["weather"][0].icon}.png`;
+        const desc = data["list"][0]["weather"][0].description;
+
+        weatherIcon.setAttribute('src', iconsrc);
+        weatherIcon.setAttribute('alt', desc);
+        captionDesc.textContent = toTitleCase(desc);
+    });
+
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
